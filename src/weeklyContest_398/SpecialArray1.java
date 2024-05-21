2343adbebe86986df0a09d283985c6fa8a92fee7
Mansi-Raj <mansi.raj.jnv@gmail.com>
2024-05-21 11:50:56 +0530

    add solution of contest 398

---
+++ src/weeklyContest_398/SpecialArray1.java
@@ -0,0 +1,26 @@
+package weeklyContest_398;
+import java.util.*;
+public class SpecialArray1 {
+    public boolean isArraySpecial(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]%2==0&&nums[i+1]%2==0||nums[i]%2!=0&&nums[i+1]%2!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n=s.nextInt();
+        System.out.print("Enter the elements of the array: ");
+        int[]num=new int[n];
+        for (int i = 0; i < n; i++) {
+            num[i]=s.nextInt();
+        }
+        SpecialArray1 sa=new SpecialArray1();
+        System.out.println(sa.isArraySpecial(num));
+        s.close();
+    }
+}
---
+++ src/weeklyContest_398/SpecialArray2.java
@@ -0,0 +1,47 @@
+package weeklyContest_398;
+import java.util.*;
+public class SpecialArray2 {
+    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+        int n = queries.length;
+        boolean[] result = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            int i1 = queries[i][0];
+            int i2 = queries[i][1];
+            boolean isSpecial = true;
+
+            for (int j = i1; j < i2; j++) {
+                if ((nums[j] % 2 == 0 && nums[j + 1] % 2 == 0) || (nums[j] % 2 != 0 && nums[j + 1] % 2 != 0)) {
+                    isSpecial = false;
+                    break;
+                }
+            }
+            result[i] = isSpecial;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter the length of array: ");
+        int n=s.nextInt();
+        System.out.print("Enter the elments of the array: ");
+        int[]nums=new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i]=s.nextInt();
+        }
+        System.out.print("Enter the length of rows of matrix: ");
+        int m=s.nextInt();
+        int[][]queries=new int[m][2];
+        System.out.print("Enter the elements of matrix: ");
+        for (int i = 0; i < m; i++) {
+            for(int j=0;j<2;j++){
+                queries[i][j]=s.nextInt();
+            }
+        }
+        SpecialArray2 sa=new SpecialArray2();
+        System.out.print(Arrays.toString(sa.isArraySpecial(nums, queries)));
+        s.close();
+    }
+}
